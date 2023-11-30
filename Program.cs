d190d0438c00ac3afd03d0a666eeaf22a5ab89b5
Ashley <144158076+Ashley-Berna-student@users.noreply.github.com>
2023-11-30 19:02:02 +0000

    done

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static Random random = new Random();
+
+    public static void Main()
+    {
+        string[] names = { "Thomas Jefferson", "John Adams", "Horatio Gates", "George Rogers Clark" };
+        string randomName = GetRandomName(names);
+        Loading();
+        Console.WriteLine($"Random Name is: {randomName}.");
+    }
+
+    public static string GetRandomName(string[] names)
+    {
+        string randomName = names[random.Next(names.Length)];
+        return randomName;
+    }
+    public static void Loading()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+}
